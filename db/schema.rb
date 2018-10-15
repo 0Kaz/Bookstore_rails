c245fcf15d607515d59831cf83dacb3294356f18
Omar Kaz <kazeb.omar@gmail.com>
2018-10-15 17:12:51 +0100

    models added and controller fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,41 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_14_163612) do
+ActiveRecord::Schema.define(version: 2018_10_15_154906) do
 
   create_table "authors", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.integer "isbn"
-    t.integer "author_id"
-    t.integer "publisher_id"
     t.integer "category_id"
+    t.integer "publisher_id"
+    t.integer "author_id"
+    t.string "isbn"
+    t.string "year"
+    t.string "price"
+    t.string "buy"
+    t.text "excerpt"
+    t.string "format"
+    t.integer "pages"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_books_on_author_id"
-    t.index ["category_id"], name: "index_books_on_category_id"
-    t.index ["publisher_id"], name: "index_books_on_publisher_id"
+    t.string "coverpath"
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "category_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "destroys", force: :cascade do |t|
-    t.string "author"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "publishers", force: :cascade do |t|
-    t.string "publisher_name"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
